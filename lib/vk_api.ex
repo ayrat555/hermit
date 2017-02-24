27e1256852d7ff70621509cf8df93d95c5c51754
Ayrat Badykov <ayratin555@gmail.com>
2017-02-24 23:41:25 +0300

    begin writing server

---
+++ lib/vk_api.ex
@@ -0,0 +1,20 @@
+defmodule Hermit.VkApi do
+  alias BalalaikaBear.Messages
+
+  def long_poll_history(server, key, ts) do
+    {:ok,
+     %{"ts" => ts,
+       "updates" => updates}} = Messages.get_long_poll_history(server, key, ts)
+
+    {ts, updates}
+  end
+
+  def long_poll_server_data(token) do
+    {:ok,
+     %{"server" => server,
+       "key" => key,
+       "ts" => ts}} = Messages.get_long_poll_server(%{access_token: token})
+
+    {server, key, ts}
+  end
+end
